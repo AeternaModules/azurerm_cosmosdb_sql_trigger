a98d568abfecd77f2c1d82b6f6735dd4d0488704
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:47:13 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,25 @@
 output "cosmosdb_sql_triggers_id" {
   description = "Map of id values across all cosmosdb_sql_triggers, keyed the same as var.cosmosdb_sql_triggers"
-  value       = { for k, v in azurerm_cosmosdb_sql_trigger.cosmosdb_sql_triggers : k => v.id }
+  value       = { for k, v in azurerm_cosmosdb_sql_trigger.cosmosdb_sql_triggers : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "cosmosdb_sql_triggers_body" {
   description = "Map of body values across all cosmosdb_sql_triggers, keyed the same as var.cosmosdb_sql_triggers"
-  value       = { for k, v in azurerm_cosmosdb_sql_trigger.cosmosdb_sql_triggers : k => v.body }
+  value       = { for k, v in azurerm_cosmosdb_sql_trigger.cosmosdb_sql_triggers : k => v.body if v.body != null && length(v.body) > 0 }
 }
 output "cosmosdb_sql_triggers_container_id" {
   description = "Map of container_id values across all cosmosdb_sql_triggers, keyed the same as var.cosmosdb_sql_triggers"
-  value       = { for k, v in azurerm_cosmosdb_sql_trigger.cosmosdb_sql_triggers : k => v.container_id }
+  value       = { for k, v in azurerm_cosmosdb_sql_trigger.cosmosdb_sql_triggers : k => v.container_id if v.container_id != null && length(v.container_id) > 0 }
 }
 output "cosmosdb_sql_triggers_name" {
   description = "Map of name values across all cosmosdb_sql_triggers, keyed the same as var.cosmosdb_sql_triggers"
-  value       = { for k, v in azurerm_cosmosdb_sql_trigger.cosmosdb_sql_triggers : k => v.name }
+  value       = { for k, v in azurerm_cosmosdb_sql_trigger.cosmosdb_sql_triggers : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "cosmosdb_sql_triggers_operation" {
   description = "Map of operation values across all cosmosdb_sql_triggers, keyed the same as var.cosmosdb_sql_triggers"
-  value       = { for k, v in azurerm_cosmosdb_sql_trigger.cosmosdb_sql_triggers : k => v.operation }
+  value       = { for k, v in azurerm_cosmosdb_sql_trigger.cosmosdb_sql_triggers : k => v.operation if v.operation != null && length(v.operation) > 0 }
 }
 output "cosmosdb_sql_triggers_type" {
   description = "Map of type values across all cosmosdb_sql_triggers, keyed the same as var.cosmosdb_sql_triggers"
-  value       = { for k, v in azurerm_cosmosdb_sql_trigger.cosmosdb_sql_triggers : k => v.type }
+  value       = { for k, v in azurerm_cosmosdb_sql_trigger.cosmosdb_sql_triggers : k => v.type if v.type != null && length(v.type) > 0 }
 }
 
